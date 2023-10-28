47b20cf593f51a7d9f2d9a25260e9578eca9d45d
Darwin Mazano <manza_92@yahoo.com>
2023-10-27 21:10:41 -0600

    agregando archivo py

--- hello.py
+++ hello.py
@@ -22,3 +22,21 @@
 left_side = 10
 right_side = 5
 left_side / right_side # 2
+
+
+'''Operador	Ejemplo
+=	x = 2
+x ahora contiene 2.
++=	x += 2
+x incrementado en 2. Si contenía 2 antes, ahora tiene un valor de 4.
+-=	x -= 2
+x reducido en 2. Si contenía 2 antes, ahora tiene un valor de 0.
+/=	x /= 2
+x dividido por 2. Si contenía 2 antes, ahora tiene un valor de 1.
+*=	x *= 2
+x multiplicado por 2. Si contenía 2 antes, ahora tiene un valor de 4.'''
+
+from datetime import date
+date.today()
+print(date.today())
+print("Today's date is: " + str(date.today()))
