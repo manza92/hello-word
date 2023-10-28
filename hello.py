bebe7a3ff6168bf4c79458effa23ce04f4082a0d
Darwin Mazano <manza_92@yahoo.com>
2023-10-27 21:04:11 -0600

    agregando archivo py

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print('Hello, World!')
+# print('Hello, World!')
+# program.py
+#sum = 1 + 2
+#print(sum)
+#print("show this in the console")
+
+
+#sum = 1 + 2 # 3
+#product = sum * 2
+#print(product)
+#planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+#distance_to_alpha_centauri = 4.367 # float, lightyears
+#can_liftoff = True
+#shuttle_landed_on_the_moon = "Apollo 11" #string
+
+#distance_to_alpha_centauri = 4.367 # looks like a float
+
+#type(distance_to_alpha_centauri) ## <class 'float'>
+
+#<left side> <operator> <right side>#
+
+left_side = 10
+right_side = 5
+left_side / right_side # 2
